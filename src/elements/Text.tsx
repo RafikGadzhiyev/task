3c28c3ea31c9268f0217b9cd0502e5de7c392843
Rafik <rafik.gadzhiyev.03@bk.ru>
2023-06-18 18:12:03 +0600

    fix: fix typo

--- src/elements/Text.tsx
+++ src/elements/Text.tsx
@@ -4,6 +4,6 @@ import { fontSize } from '../consts/styles';
 
 export const Text = styled.span<ITextProps>`
     color: ${(props) => props.color};
-    font-size: ${(props) => fontSize[props.fontSize]};
+    font-size: ${(props) => fontSize[props.fontSize || 'md']};
     font-style: ${props => props.fontStyle};
 `;
